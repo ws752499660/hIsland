c2e08122c36c3b1c0278240279f539736dc85944
ws752499660 <ws752499660@foxmail.com>
2018-12-24 20:01:03 +0800

    前端bug基本修复完成

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -67,11 +67,6 @@ new Vue({
       commentAddFlag:null,
 
 
-      commentForAdd:{
-          authorId:null,
-          topicId:null,
-          content:null
-      },
 
       delCommentObj:{
           userId:"",
@@ -99,13 +94,30 @@ new Vue({
       getCommentObjList:[],
       getCommentObj:{
           topicId:null,
-          commentList:null
+          commentList:[]
       }
     },
     methods:{
         show3:function(){
           this.show3v=!this.show3v
         },
+
+        getTopicList:function(){
+            var xhr=new XMLHttpRequest();
+            var url="http://127.0.0.1:8080/getTopicList";
+            xhr.open("get",url,false);
+            xhr.send();
+            console.log("获取topic列表-发送数据：","不需要发送数据，只请求get一下就好");
+            recive=JSON.parse(xhr.response);
+            console.log("获取topic列表-接受数据：",recive);
+            recive.forEach(function (e) {
+                e.date=new Date(e.date);
+                e.date=e.date.getFullYear()+"-"+e.date.getMonth()+"-"+e.date.getDate()+"  "+e.date.getHours()+":"+e.date.getMinutes();
+                console.log("获取topic列表-这里对date进行格式化为Date类型后再次格式化为我们想要的格式",e.date);
+            })
+            this.topicList=recive; 
+        },
+
         handleCommentObj:function(){
             this.getCommentObjList=new Array(this.topicList.length);
            
@@ -113,7 +125,7 @@ new Vue({
             for(var i=0;i<this.topicList.length;i++){
                     this.getCommentObjList[i]={
                         topicId:this.topicList[i].id,
-                        commentList:null
+                        commentList:[]
                     }
                     console.log("topicId",this.topicList[i].id)
             }
@@ -126,13 +138,19 @@ new Vue({
             if(val.length!=0){
                 this.loginStatus.topicNum=this.topicList[val].id;
                 this.loginStatus.delName=this.topicList[val].authorId;
+            
+            // for(var i=0;i<val.length;i++){
+            //     var index=val[i];
+            //     console.log(this.getCommentObjList[index].topicId);//测试是否能将输出当前序号前的所有topic
+            //     var commentList=this.getCommentList(this.getCommentObjList[index].topicId);
+            //     this.getCommentObjList[index].commentList=commentList;
+            //     console.log("commentlisttest",this.getCommentObjList[index].commentList);
+            // } 
+                console.log(this.getCommentObjList[val].topicId);//测试是否能将输出当前序号前的所有topic
+                var commentList=this.getCommentList(this.getCommentObjList[val].topicId);
+                this.getCommentObjList[val].commentList=commentList;
+                console.log("commentlisttest",this.getCommentObjList[val].commentList);
             }
-            for(i in val){
-                var index=val[i]
-                console.log(this.getCommentObjList[index].topicId);//测试是否能将输出当前序号前的所有topic
-                var commentList=this.getCommentList(this.getCommentObjList[index].topicId);
-                this.getCommentObjList[index].commentList=commentList;
-            }       
         },
         getCommentList:function (topicId) {
             var xhr=new XMLHttpRequest();
@@ -154,6 +172,8 @@ new Vue({
             var url="http://127.0.0.1:8080/userRegister";
             xhr.open("post",url,false);
             xhr.setRequestHeader("content-type","application/json");
+            this.user.group="N";
+            this.user.states="N";
             var text=JSON.stringify(this.user);
             xhr.send(text);
             console.log("用户注册-发送数据：",text);
@@ -175,6 +195,9 @@ new Vue({
             console.log("登录状态" ,this.loginFlag.flag);
             if( this.loginFlag.flag==true){
                  this.loginStatus.id=this.loginObj.id;
+                 this.id=this.loginObj.id;
+                 this.getUserName();
+                 this.loginStatus.name=this.name;
                  console.log("获取用户id",this.loginStatus.id);
             }
         },
@@ -192,6 +215,13 @@ new Vue({
             recive=JSON.parse(xhr.response);
             console.log("获取用户姓名-接受数据：",recive);
             this.name=recive.name;
+        },
+
+        addTopicClk:function(){
+            this.addTopic();
+            this.getTopicList();
+            this.handleCommentObj();
+            this.addtopicVisible=false;
         },
 
         addTopic:function () {
@@ -212,6 +242,34 @@ new Vue({
             this.topicAddFlag=recive;
         },
         /**需要增加topicId */
+
+        addCommentClk:function(){
+            this.addComment();
+            if(this.commentAddFlag.flag){
+                for(var i in this.getCommentObjList){
+                    var item=this.getCommentObjList[i];
+                    if(item.topicId==this.commentForAdd.topicId)
+                    {
+                        var newComment={
+                            authorId:this.commentForAdd.authorId,
+                            authorName:this.loginStatus.name,
+                            topicId:this.commentForAdd.topicId,
+                            content:this.commentForAdd.content,
+                            id:this.commentAddFlag.commentId
+                        }
+                        item.commentList.push(newComment);
+                        console.log("ac",item);
+                    }
+                }
+            }
+            this.commentForAdd={
+                authorId:null,
+                topicId:null,
+                content:null
+            };
+            this.commentAddFlag=null;
+            this.addCommentVisible=false;
+        },
         
         addComment:function () {
          
@@ -222,7 +280,6 @@ new Vue({
 
             this.commentForAdd.authorId=this.loginStatus.id;
             this.commentForAdd.topicId=this.loginStatus.topicNum;//这句需要改
-            this.commentForAdd.content=this.commentReceive.content;
 
             var text=JSON.stringify(this.commentForAdd);
             xhr.send(text);
@@ -231,7 +288,17 @@ new Vue({
             console.log("评论-接受数据：",recive);
             this.commentAddFlag=recive;
         },
-
+        delTopicClk:function(){
+            this.delTopic();
+            // for(var i in this.topicList){
+            //     if(this.topicList[i].id==this.delTopicObj.topicId){
+            //         this.topicList.splice(i);
+            //     }
+            // }
+            this.getTopicList();
+            this.handleCommentObj();
+            this.activeNames=[];
+        },
         /*需要增加topicID*/
         delTopic:function () {
             var xhr=new XMLHttpRequest();
@@ -239,7 +306,7 @@ new Vue({
             xhr.open("post",url,false);
             xhr.setRequestHeader("content-type","application/json");
 
-            this.delTopicObj.userId=this.loginStatus.delName;
+            this.delTopicObj.userId=this.loginStatus.id;
             this.delTopicObj.topicId=this.loginStatus.topicNum;
             var text=JSON.stringify(this.delTopicObj);
             xhr.send(text);
@@ -248,6 +315,19 @@ new Vue({
             console.log("删主题-接受数据：",recive);
             this.delTopicFlag=recive;
         },
+
+        delCommentClk:function(commentId,key,index){
+            console.log("comment",commentId);
+            this.delCommentObj.userId=this.loginStatus.id;
+            this.delCommentObj.commentId=commentId;
+            this.delComment();
+            var commentObj=this.getCommentObjList[key];
+            console.log("dc1",commentObj);
+            commentObj.commentList.splice(index);
+            console.log("dc2",commentObj);
+            this.activeNames=[];
+        },
+
         delComment:function () {
             var xhr=new XMLHttpRequest();
             var url="http://127.0.0.1:8080/delComment";
@@ -260,7 +340,9 @@ new Vue({
             console.log("删评论-接受数据：",recive);
             this.delCommentFlag=recive;
         },
+        
         banUser:function () {
+            this.banUserObj.optUserId=this.loginStatus.id;
             var xhr=new XMLHttpRequest();
             var url="http://127.0.0.1:8080/banUser";
             xhr.open("post",url,false);
@@ -275,19 +357,7 @@ new Vue({
 
     },      
     created:function(){
-        var xhr=new XMLHttpRequest();
-        var url="http://127.0.0.1:8080/getTopicList";
-        xhr.open("get",url,false);
-        xhr.send();
-        console.log("获取topic列表-发送数据：","不需要发送数据，只请求get一下就好");
-        recive=JSON.parse(xhr.response);
-        console.log("获取topic列表-接受数据：",recive);
-        recive.forEach(function (e) {
-            e.date=new Date(e.date);
-            e.date=e.date.getFullYear()+"-"+e.date.getMonth()+"-"+e.date.getDate()+"  "+e.date.getHours()+":"+e.date.getMinutes();
-            console.log("获取topic列表-这里对date进行格式化为Date类型后再次格式化为我们想要的格式",e.date);
-        })
-        this.topicList=recive; 
-        this.handleCommentObj()
+        this.getTopicList();
+        this.handleCommentObj();
     }
 })
